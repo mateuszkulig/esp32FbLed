71e91ddd643d9e7ccc59e08f1173af6f412e190b
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-07-17 22:17:24 +0200

    remove one useless define

--- main/main.c
+++ main/main.c
@@ -6,12 +6,12 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 
-static const char *TAG = "example";
+static const char *TAG = "boobs";
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+
 
 static uint8_t s_led_state = 0;
 
@@ -37,7 +37,7 @@ static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
     /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
+    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, CONFIG_BLINK_GPIO, 1);
     /* Set all LED off to clear all pixels */
     pStrip_a->clear(pStrip_a, 50);
 }

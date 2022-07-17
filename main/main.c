dda01e2c1e772d61c51f7e5b0d1ecc34b63d1bf0
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-07-17 22:41:15 +0200

    created 2 functions to shine rgb and turn off instead of one to blink

--- main/main.c
+++ main/main.c
@@ -8,34 +8,30 @@
 
 static const char *TAG = "boobs";
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-
-
 static uint8_t s_led_state = 0;
 
 static led_strip_t *pStrip_a;
 
 
-static void blink_led(void)
+static void led_on(uint8_t r, uint8_t g, uint8_t b)
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        pStrip_a->set_pixel(pStrip_a, 0, 51, 204, 51);
-        /* Refresh the strip to send data */
-        pStrip_a->refresh(pStrip_a, 100);
-    } else {
-        /* Set all LED off to clear all pixels */
-        pStrip_a->clear(pStrip_a, 50);
-    }
+    ESP_LOGI(TAG, "turning the led on, colored (%d, %d, %d)rgb!", r, g, b);
+    /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+    pStrip_a->set_pixel(pStrip_a, 0, r, g, b);
+    /* Refresh the strip to send data */
+    pStrip_a->refresh(pStrip_a, 100);
+}
+
+static void led_off(void) {
+    ESP_LOGI(TAG, "turning the led off");
+    /* Set all LED off to clear all pixels */
+    pStrip_a->clear(pStrip_a, 50);
 }
 
 
 static void configure_led(void)
 {
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    ESP_LOGI(TAG, "Program switching built-in led's color from messenger");
     /* LED strip initialization with the GPIO and pixels number*/
     pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, CONFIG_BLINK_GPIO, 1);
     /* Set all LED off to clear all pixels */
@@ -47,13 +43,7 @@ void app_main(void)
 {
     /* Configure the peripheral according to the LED type */
     configure_led();
-    uint16_t i = 0;
-    uint16_t times = 5;
-    for(i=0; i<(times*2)+1; i++) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    }
+    led_on(255, 26, 140);
+    vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    led_off();
 }

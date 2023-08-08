58516a6098b627a89070aebe32fdca13cda6c60d
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-08-08 19:01:43 +0200

    change formatting in main

--- main/main.c
+++ main/main.c
@@ -6,13 +6,23 @@
 
 static const char *TAG = "boobs";
 
-void startup_sequence(void) {
+/**
+ * @brief   Function always running first after startup
+ * @note    configure_led function should have different naming
+ * 
+ */
+void startupSequence(void) {
     /* display rainbow on startup to test led */
     configure_led();
 }
 
+/**
+ * @brief   Entry point for ESP-IDF
+ *          The name cannot to be changed to camelCase because framework is searching the function with this exact name
+ * 
+ */
 void app_main(void)
 {
     /* Configure the peripheral according to the LED type */
-    startup_sequence();
+    startupSequence();
 }
